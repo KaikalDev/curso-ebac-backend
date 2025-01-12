18b90d81722a0a492599cb9a7ac4ba943d6e5ece
Kaique <kaiquesimoes58@gmail.com>
2025-01-12 12:02:06 -0300

    Atividade_modulo_22

--- atividade_modulo_22/src/Main.java
+++ atividade_modulo_22/src/Main.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Pessoa> mas = new ArrayList<>();
-        List<Pessoa> fem = new ArrayList<>();
+        List<Pessoa> pessoas = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         boolean loop = true;
@@ -13,34 +13,25 @@ public class Main {
             System.out.println("Insira o nome e o sexo: (nome,sexo) ou digite 'sair' para encerrar: ");
             String input = sc.nextLine();
 
-            if(input.equalsIgnoreCase("sair")) {
+            if (input.equalsIgnoreCase("sair")) {
                 loop = false;
                 break;
             }
 
             String[] res = input.split(",");
-            if (res.length == 2) {
+            if (res.length == 2 && (res[1].equalsIgnoreCase("masculino") || res[1].equalsIgnoreCase("feminino"))) {
                 Pessoa pess = new Pessoa(res[0].trim(), res[1].trim());
-                if(pess.getSexo().equalsIgnoreCase("masculino")) {
-                    mas.add(pess);
-                } else if(pess.getSexo().equalsIgnoreCase("feminino")) {
-                    fem.add(pess);
-                } else {
-                    System.out.println("Sexo inválido. Use 'masculino' ou 'feminino'.");
-                }
+                pessoas.add(pess);
             } else {
                 System.out.println("Entrada inválida. Certifique-se de inserir no formato 'nome,sexo'.");
             }
         } while (loop);
 
-        System.out.println("Nomes masculinos:");
-        for(Pessoa p : mas) {
-            System.out.println(p.getNome());
-        }
+        List<Pessoa> fem = pessoas.stream()
+                .filter(p -> p.getSexo().equalsIgnoreCase("feminino"))
+                .collect(Collectors.toList());
 
         System.out.println("Nomes femininos:");
-        for(Pessoa p : fem) {
-            System.out.println(p.getNome());
-        }
+        fem.forEach(p -> System.out.println(p.getNome()));
     }
 }
